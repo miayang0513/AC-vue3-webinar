12e83aac08b40ebfd94dbb5592e8cf81282ecab6
miayang0513 <a16168972@gmail.com>
2022-03-20 23:57:38 +0800

    Example: Setup

---
+++ src/views/ExampleSetup.vue
@@ -0,0 +1,57 @@
+<template>
+  <h1>{{ title }}</h1>
+  <button @click="increment">{{ count }}</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  props: {
+    count: Number
+  },
+  emits: ['increment'],
+  setup (props, context) {
+    // Accessing Props
+    console.log(props.count)
+
+    // Composition API
+    const title = ref('Hello World!')
+
+    const increment = () => {
+      context.emit('increment')
+    }
+
+    return {
+      title,
+      increment
+    }
+  }
+}
+</script>
+
+<!-- Option API -->
+
+<!-- <template>
+  <h1>{{ title }}</h1>
+  <button @click="increment">{{ count }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    count: Number
+  },
+  data() {
+    return {
+      title: 'Hello World!'
+    }
+  },
+  methods: {
+    increment() {
+      this.$emit('increment')
+    }
+  }
+}
+</script> -->
+
