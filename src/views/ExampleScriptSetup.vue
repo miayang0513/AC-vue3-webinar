98f9d916fe06e499cabeac43b132a012eb548e14
miayang0513 <a16168972@gmail.com>
2022-03-21 00:20:42 +0800

    Example: Script Setup

---
+++ src/views/ExampleScriptSetup.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ title }}</h1>
+  <button @click="increment">{{ count }}</button>
+  <my-component/>
+</template>
+
+<script setup>
+import MyComponent from './MyComponent.vue'
+import { ref, defineProps, defineEmits } from 'vue'
+
+const title = ref('Hello World!')
+
+const props = defineProps({
+  count: Number
+})
+
+console.log(props.count)
+
+const emit = defineEmits(['increment'])
+
+const increment = () => {
+  emit('increment')
+}
+</script>
+
